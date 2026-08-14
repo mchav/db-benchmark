85a8d8ef3af4ab7b972981d1aee9413f749b2d65
Michael Chavinda <mschavinda@gmail.com>
2026-08-13 23:51:40 -0700

    fix: Do not hard cap runner memory at 30GB
    Rather cap relative to available memory so runs work in x-large env.

--- haskell/exec.sh
+++ haskell/exec.sh
@@ -24,4 +24,14 @@ fi
 source ./haskell/ver-haskell.sh
 
 [ -d "$HOME/.cache/cabal/packages/hackage.haskell.org" ] || cabal update
+
+## Set GHC's memory limit to 90% of the machine's memory so
+## we exit gracefully.
+if [ -r /proc/meminfo ]; then
+    MEM_KB=$(awk '/^MemTotal:/ {print $2}' /proc/meminfo)
+else
+    MEM_KB=$(( $(sysctl -n hw.memsize) / 1024 ))
+fi
+export GHCRTS="-M$(( MEM_KB * 9 / 10 / 1024 ))m"
+
 cabal --project-dir=./haskell run -O2 "$1-haskell"
